2d7228bb428ef3a22ad2f3bfb933d2d940760ee7
rohith illuri <56576663+rohithIlluri@users.noreply.github.com>
2023-03-30 16:05:02 -0500

    connections

---
+++ employee-management-system-ui/src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import AddEmployee from "./components/AddEmployee";
+
+export default function App() {
+  return (
+    <>
+      <Navbar/>
+      <AddEmployee/>
+    </>
+  )
+}
---
+++ employee-management-system-ui/src/components/AddEmployee.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AddEmployee = () => {
+  return (
+    <div className='flex max-w-2xl mx-auto shadow border-b'>
+        <div className='px-8 py-8'>
+            <div className='font-thin text-2xl tracking-wider'>
+                <h1>Add New Employee</h1>
+            </div>
+            <div className='items-center justify-center h-14 w-full'>
+                <label>First Name</label>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddEmployee
